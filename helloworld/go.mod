01a5103c563ae51dd73ce1c761967b150971a887
chrptos <chrptos@gmail.com>
2022-08-26 19:51:30 +0900

    add: home,about handler

---
+++ helloworld/go.mod
@@ -0,0 +1,3 @@
+module github.com/chrptos/helloworld
+
+go 1.18
--- helloworld/main.go
+++ helloworld/main.go
@@ -5,14 +5,29 @@ import (
 	"net/http"
 )
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		n, err := fmt.Fprintf(w, "Hello, World!")
-		if err != nil {
-			fmt.Println(err)
-		}
-		fmt.Println(fmt.Sprintf("Number of byte written: %d", n))
-	})
+const portNumber = ":8080"
 
-	_ = http.ListenAndServe(":8080", nil)
+// Home is the handler for the home page
+func Home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "This is the home page")
+}
+
+// About is the handler for the about page
+func About(w http.ResponseWriter, r *http.Request) {
+	sum := addValues(2, 2)
+	_, _ = fmt.Fprintf(w, fmt.Sprintf("This is the about page and 2 + 2 is %d", sum))
+}
+
+// addValues adds two ints x and y, and returns the sum
+func addValues(x, y int) int {
+	return x + y
+}
+
+// main is the main function
+func main() {
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/about", About)
+
+	fmt.Println(fmt.Sprintf("Staring application on port %s", portNumber))
+	_ = http.ListenAndServe(portNumber, nil)
 }
