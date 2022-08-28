b91b46121ee31bafd6e5b360dff03337fc2ea222
chrptos <chrptos@gmail.com>
2022-08-28 18:46:57 +0900

    refactoring: 機能ごとにフォルダ分け、パッケージ管理にする

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # go-projects
+
+### go path extension
+- pathを自動的に解決してくれる（めちゃくちゃ便利）
+https://marketplace.visualstudio.com/items?itemName=christian-kohler.path-intellisense
--- helloworld/package/handlers/handlers.go
+++ helloworld/package/handlers/handlers.go
@@ -1,18 +1,20 @@
-package main
+package handlers
 
 import (
 	"net/http"
+
+	"github.com/chrptos/helloworld/package/render"
 )
 
 // Home is the handler for the home page
 func Home(w http.ResponseWriter, r *http.Request) {
 	// fmt.Fprintf(w, "This is the home page")
-	renderTemplate(w, "home.page.tmpl")
+	render.RenderTemplate(w, "home.page.tmpl")
 }
 
 // About is the handler for the about page
 func About(w http.ResponseWriter, r *http.Request) {
 	// sum := addValues(2, 2)
 	// _, _ = fmt.Fprintf(w, fmt.Sprintf("This is the about page and 2 + 2 is %d", sum))
-	renderTemplate(w, "about.page.tmpl")
+	render.RenderTemplate(w, "about.page.tmpl")
 }
