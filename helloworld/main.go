5fb369f4aca4bf1e3bf8f3875def179ff73fe0fb
chrptos <chrptos@gmail.com>
2022-08-28 18:14:57 +0900

    templateファイルの導入

--- helloworld/main.go
+++ helloworld/main.go
@@ -3,24 +3,36 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"text/template"
 )
 
 const portNumber = ":8080"
 
 // Home is the handler for the home page
 func Home(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "This is the home page")
+	// fmt.Fprintf(w, "This is the home page")
+	renderTemplate(w, "home.page.tmpl")
 }
 
 // About is the handler for the about page
 func About(w http.ResponseWriter, r *http.Request) {
-	sum := addValues(2, 2)
-	_, _ = fmt.Fprintf(w, fmt.Sprintf("This is the about page and 2 + 2 is %d", sum))
+	// sum := addValues(2, 2)
+	// _, _ = fmt.Fprintf(w, fmt.Sprintf("This is the about page and 2 + 2 is %d", sum))
+	renderTemplate(w, "about.page.tmpl")
 }
 
 // addValues adds two ints x and y, and returns the sum
 func addValues(x, y int) int {
 	return x + y
+}
+
+func renderTemplate(w http.ResponseWriter, tmpl string) {
+	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
+	err := parsedTemplate.Execute(w, nil)
+	if err != nil {
+		fmt.Println("Error parsing template:", err)
+		return
+	}
 }
 
 // main is the main function
