95aafdfc6b215f2f98f4708a0c900e1364d69cd3
chrptos <chrptos@gmail.com>
2022-08-28 19:01:17 +0900

    add: base layout template

--- helloworld/package/render/render.go
+++ helloworld/package/render/render.go
@@ -7,7 +7,10 @@ import (
 )
 
 func RenderTemplate(w http.ResponseWriter, tmpl string) {
-	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
+	// 文字列でレンダリングしたいテンプレートファイルを受け取る
+	// baseテンプレートも含まなければいけない
+	// TODO: ディスクから読み取るのをやめる
+	parsedTemplate, _ := template.ParseFiles("./templates/"+tmpl, "./templates/base.layout.tmpl")
 	err := parsedTemplate.Execute(w, nil)
 	if err != nil {
 		fmt.Println("Error parsing template:", err)
