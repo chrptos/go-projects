dddfb820e06639605c123003af33544769692c2c
chrptos <chrptos@gmail.com>
2022-09-16 20:42:58 +0900

    パッケージ分割

--- myapi/main.go
+++ myapi/main.go
@@ -1,26 +1,20 @@
 package main
 
 import (
-	"io"
 	"log"
 	"net/http"
+
+	"github.com/chrptos/myapi/handlers"
 )
 
 func main() {
-	// httpリクエストを受けて、それに対するhttpレスポンス内容をコネクションに書き込む
-	// helloHandlerという名前で、ハンドラを定義
-	helloHandler := func(w http.ResponseWriter, req *http.Request) {
-		io.WriteString(w, "Hello, World!!\n")
-	}
-	// 定義したhelloHandlerを使うように登録する
-	http.HandleFunc("/hello", helloHandler)
+	http.HandleFunc("/hello", handlers.HelloHandler)
+	http.HandleFunc("/article", handlers.PostArticleHandler)
+	http.HandleFunc("/article/list", handlers.ArticleListHandler)
+	http.HandleFunc("/article/1", handlers.ArticleDetailHandler)
+	http.HandleFunc("/article/nice", handlers.PostNiceHandler)
+	http.HandleFunc("/comment", handlers.PostCommentHandler)
+
 	log.Println("server start at port 8080")
-
-	// サーバーの起動
-	// プログラムが終了するような重大なエラーが返ってきた場合に（ListenAndServeの返り値がエラー）、該当エラーを出力し処理を終了する（log.Fatal）
 	log.Fatal(http.ListenAndServe(":8080", nil))
-
-	// 1. ハンドラを作成する
-	// 2. ハンドラとパスを結びつける
-	// 3. サーバーを起動する
 }
