a454d3d589b4d0040e4fad2c5177585b969a7397
chrptos <chrptos@gmail.com>
2022-10-08 21:47:42 +0900

    ストリームのデコード処理を追加

--- myapi/handlers/handlers.go
+++ myapi/handlers/handlers.go
@@ -2,7 +2,6 @@ package handlers
 
 import (
 	"encoding/json"
-	"errors"
 	"fmt"
 	"io"
 	"net/http"
@@ -14,33 +13,11 @@ import (
 
 // /article のハンドラ
 func PostArticleHandler(w http.ResponseWriter, req *http.Request) {
-	// バイトスライス reqBodybufferを何らかの形で用意
-	// req.HeaderのGetメソッドで得られたContent-Lengthはstring型なので数値に変換する
-	length, err := strconv.Atoi(req.Header.Get("Content-Length"))
-	if err != nil {
-		http.Error(w, "cannot get content length\n", http.StatusBadRequest)
-		return
-	}
-	// 得られたlength分のバイト配列を作成する
-	reqBodybuffer := make([]byte, length)
-	// var reqBodybuffer []byte
-
-	// ReadCloser(Reader+Closer)
-	// Readメソッド：ボディの中身を、引数として渡したバイトスライスに格納する
-	// Closeメソッド：ボディの中身を読み終わった時にクローズする
-	if _, err := req.Body.Read(reqBodybuffer); !errors.Is(err, io.EOF) {
-		// errorsパッケージのIs関数で、err と io.EOFが等しいか判定する（EOFは読み切ったかどうか
-		http.Error(w, "fail to get request body\n", http.StatusBadRequest)
-		return
-	}
-	// クローズ処理
-	defer req.Body.Close()
-
 	// json→Article構造体へデコード処理
 	var reqArticle models.Article
-	if err := json.Unmarshal(reqBodybuffer, &reqArticle); err != nil {
+
+	if err != json.NewDecoder(req.Body).Decode(&reqArticle); err != nil {
 		http.Error(w, "fail to decode json\n", http.StatusBadRequest)
-		return
 	}
 
 	// Article構造体→jsonエンコード処理
