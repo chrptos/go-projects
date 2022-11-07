ec9dab02f3e5835edae99c16c8e355c73c185e50
chrptos <chrptos@gmail.com>
2022-11-07 22:57:54 +0900

    add test

---
+++ myapi/repositories/article_test.go
@@ -0,0 +1,23 @@
+package repositories_test
+
+import (
+	"testing"
+
+	"github.com/chrptos/myapi/models"
+	"github.com/chrptos/myapi/repositories"
+)
+
+func TestSelectArticleDetail(t *testing.T) {
+	expected := models.Article{
+		//
+	}
+
+	got, err := repositories.SelectArticleDetail()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if got != expected {
+		t.Errorf("get %s but want %s\n", got, expected)
+	}
+}
