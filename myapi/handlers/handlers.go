9486a67cc1ebad579a3ca9fa54f154ee6398d30a
chrptos <chrptos@gmail.com>
2022-12-19 17:39:24 +0900

    1-6

--- myapi/handlers/handlers.go
+++ myapi/handlers/handlers.go
@@ -21,7 +21,22 @@ func PostArticleHandler(w http.ResponseWriter, req *http.Request) {
 
 // article/list のハンドラ（GET: ブログ一覧を取得）
 func ArticleListHandler(w http.ResponseWriter, req *http.Request) {
-	io.WriteString(w, "Article List\n")
+	queryMap := req.URL.Query()
+
+	var page int
+	if p, ok := queryMap["page"]; ok && len(p) > 0 {
+		var err error
+		page, err = strconv.Atoi(p[0])
+		if err != nil {
+			http.Error(w, "Invalid query parameter", http.StatusBadRequest)
+			return
+		}
+	} else {
+		page = 1
+	}
+
+	resString := fmt.Sprintf("Artcle list (page %d)\n", page)
+	io.WriteString(w, resString)
 }
 
 // article/1 のハンドラ（GET: 指定したIDのブログ記事を取得する）
